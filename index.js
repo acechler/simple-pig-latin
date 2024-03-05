55265e6fcd55b3df70647e9f6571710bb7178ba5
Aaron Echler <44124054+acechler@users.noreply.github.com>
2024-03-05 18:24:26 -0500

    Pseudo Code

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 // https://www.codewars.com/kata/520b9d2ad5c005041100000f/train/javascript
 
+
+///===== Pig latin examples=================
+//
+//  Pig latin is cool  ->   igPay atinlay siay oolcay  
+//  Hello world        ->   elloHay orldway !
+//
+//
+//  The goal of this exercise will be to append 'ay' to the end of each word.
+//  The first letter will have to go before 'ay'.
+//
+//  The best way to do this is to treat this like a queue or stack. 
+//  Maybe pop out the character and push it back in. 
+//
+//  I will need to determine how to split all this.
+//  I may need to parse through the word. That will be what I do first.
+//  I will have to research some javascript utilities for any easier way.
 function pigIt(str){
     //Code here
 }
